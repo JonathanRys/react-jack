f0ca8b720f7b39615f9510107fafc26bc28fc7d3
Jonathan Rys <jonathan.rk.rys@gmail.com>
2017-12-30 09:09:08 -0500

    Fix buy Insurance

--- src/ControlPanel/ControlPanel.js
+++ src/ControlPanel/ControlPanel.js
@@ -6,24 +6,8 @@ import './control_panel.css'
 import { Button, Form, Label } from 'reactstrap'
 
 export default class ControlPanel extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            dealHandler: () => {
-                props.dealOnClick()
-                if (props.dealer.hand.length < 2 || props.player.hands[0].length < 2) {
-                    console.log("Give them cards:")
-                    props.keepDealing()
-                }
-            }
-        }
-        console.log("props in the CP", props)
-    }
-
     componentWillReceiveProps(nextProps) {
         if (nextProps.dealer.hand.length < 1 || nextProps.player.hands[0].length < 2) {
-            console.log("They still need cards:", nextProps.dealer.hand.length, nextProps.player.hands[0].length)
             nextProps.keepDealing()
         }
     }
@@ -34,17 +18,19 @@ export default class ControlPanel extends Component {
             if (!this.props.turn.isPlaying) return false
             if (!this.props.dealer.hand[0]) return false
             if (this.props.dealer.hand[0].slice(1) !== "A") return false
-            if (this.props.player.hasInsurance[this.props.player.handIndex]) return false
+            if (!this.props.player.hasInsurance[this.props.player.handIndex]) return false
+            return true;
         }
+
         return (
-            <Form xs="10" sm="8" md="6" lg="3" className="ControlPanel_main">
+            <Form xs="4" sm="6" md="8" lg="12" className="ControlPanel_main">
                 <Label className="ControlPanel_main_panel-header">Control Panel</Label>
                 <Button size="md" color="secondary" block disabled={!this.props.turn.isPlaying} onClick={this.props.hitOnClick}>Hit</Button>
                 <Button size="md" color="secondary" block disabled={!this.props.turn.isPlaying} onClick={this.props.standOnClick}>Stand</Button>
                 <Button size="md" color="secondary" block disabled={!canSplit} onClick={this.props.splitOnClick}>Split</Button>
                 <Button size="md" color="secondary" block disabled={this.props.player.hands[0].length !== 2} onClick={this.props.doubleDownOnClick}>Double Down</Button>
                 <Button size="md" color="secondary" block disabled={!canBuyInsurance()} onClick={this.props.buyInsuranceOnClick}>Buy Insurance</Button>
-                <Button size="md" color="primary" block disabled={this.props.turn.isPlaying} onClick={this.state.dealHandler}>Deal</Button>
+                <Button size="md" color="primary" block disabled={this.props.turn.isPlaying} onClick={this.props.dealOnClick}>Deal</Button>
             </Form>
         )
     }
@@ -53,5 +39,4 @@ export default class ControlPanel extends Component {
 ControlPanel.propTypes = {
     drawnCard: PropTypes.string,
     isPlaying: PropTypes.bool,
-
 }
